8bf46105c980a228bd101890e3d4288ec848e04e
Silent933 <90222236+Silent933@users.noreply.github.com>
2026-06-12 02:16:37 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+  return <>
+    <style>{`*,*::before,*::after{margin:0;padding:0;box-sizing:border-box}body{min-height:100vh}`}</style>
+    <Component {...pageProps} />
+  </>;}
